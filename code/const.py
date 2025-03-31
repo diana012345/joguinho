c548f36a12df1be3a82e8955f8a7f4a64ef5b3a0
Lenovo <stvnsdiana.o@gmail.com>
2025-03-31 14:43:52 -0300

    new

---
+++ code/const.py
@@ -0,0 +1,11 @@
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (57, 255, 20)
+
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+
+MENU_OPTION = ('New Game 1P',
+               'New Game 2P - COOP',
+               'New Game 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
--- code/game.py
+++ code/game.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
+from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self, ):
-        pass
+    def run(self):
+
+
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+            #for event in pygame.event.get():
+             #   if event.type == pygame.QUIT:
+              #      pygame.quit()  # close window
+               #     quit()  # end pg
+
+
+
+
+
